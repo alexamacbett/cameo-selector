7dd940009181304f9a69d63802ddb8fae54ae58b
Alexander Bett <abett01@qub.ac.uk>
2022-01-29 17:42:27 +0000

    Initial commit. 1.0.1 version

---
+++ Plugin.cs
@@ -0,0 +1,169 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Sunless.Game.ApplicationProviders;
+using System;
+using System.Collections.Generic;
+using Sunless.Game.Utilities;
+using Sunless.Game.UI.Officers;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Linq;
+using Sunless.Game.Entities;
+
+
+namespace CCCameoSelector
+{
+    [BepInPlugin("mod.clevercrumbish.cameoselector","Cameo Selector", "1.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Loaded {PluginInfo.PLUGIN_GUID}!");
+            DoPatching();
+        }
+
+        public static void DoPatching()
+        {
+            Harmony.CreateAndPatchAll(typeof(AvatarAndNamePanelConstructorPatch));
+        }
+    }
+
+    [HarmonyPatch(typeof(AvatarAndNamePanel), MethodType.Constructor, new System.Type[1] { typeof(GameObject) })]
+    class AvatarAndNamePanelConstructorPatch
+    {
+
+
+        static void Postfix(AvatarAndNamePanel __instance)
+        {
+            AvatarAndNamePanelHelper.ClearPage(__instance);
+            AvatarAndNamePanelHelper.Initialise(__instance);
+            AvatarAndNamePanelHelper.LoadPage(__instance);
+        }
+
+    }
+
+    public static class AvatarAndNamePanelHelper
+    {
+        public static int pageNumber = 0;
+        public static int maxPageNumber = 0;
+        public static List<List<string>> pages = new List<List<string>>();
+        public static void Initialise(AvatarAndNamePanel __instance)
+        {
+            List<String> looseAvatars = new List<string>();
+            looseAvatars.AddRange(ImageHelper.GetAvatarNames());
+            looseAvatars.AddRange(GetAdditionalAvatarNames());
+            int offset = 0;
+            int currentPageSize;
+            if (looseAvatars.Count <= 21)
+            {
+                pages.Add(new List<string>());
+                pages[0].AddRange(looseAvatars);
+                offset = looseAvatars.Count;
+            }
+            while (offset < looseAvatars.Count)
+            {
+                while (pages.Count < pageNumber + 1)
+                {
+                    pages.Add(new List<string>());
+                }
+                if (pageNumber == 0)
+                {
+                    currentPageSize = 20;
+                }
+                else if ((looseAvatars.Count - offset) <= 20)
+                {
+                    currentPageSize = looseAvatars.Count - offset;
+                }
+                else
+                {
+                    currentPageSize = 19;
+                }
+                pages[pageNumber].AddRange(looseAvatars.Skip(offset).Take(currentPageSize));
+                offset += currentPageSize;
+                if (offset < looseAvatars.Count)
+                {
+                    pageNumber++;
+                }
+            }
+            maxPageNumber = pageNumber;
+            pageNumber = 0;
+        }
+
+        public static void CreateBackButton(AvatarAndNamePanel __instance)
+        {
+            GameObject gameObject = __instance._avatarGrid.AddChildPreserveTransform(PrefabHelper.Instance.Get("UI/CharacterCreation/AvatarButton"));
+            gameObject.GetComponentInChildren<RawImage>().texture = (Texture)"avatars/cc_menu/cc_menu_left".GetIconTexture("");
+            gameObject.GetComponent<Button>().OnClick((Action)(() => AvatarAndNamePanelHelper.PreviousPage(__instance)));
+        }
+
+        public static void CreateForwardButton(AvatarAndNamePanel __instance)
+        {
+            GameObject gameObject = __instance._avatarGrid.AddChildPreserveTransform(PrefabHelper.Instance.Get("UI/CharacterCreation/AvatarButton"));
+            gameObject.GetComponentInChildren<RawImage>().texture = (Texture)"avatars/cc_menu/cc_menu_right".GetIconTexture("");
+            gameObject.GetComponent<Button>().OnClick((Action)(() => AvatarAndNamePanelHelper.NextPage(__instance)));
+        }
+
+        public static void CreateBlankButton(AvatarAndNamePanel __instance)
+        {
+            GameObject gameObject = __instance._avatarGrid.AddChildPreserveTransform(PrefabHelper.Instance.Get("UI/CharacterCreation/AvatarButton"));
+            gameObject.GetComponentInChildren<RawImage>().texture = (Texture)"avatars/cc_menu/cc_menu_blank".GetIconTexture("");
+        }
+
+        public static void ClearPage(AvatarAndNamePanel __instance)
+        {
+            __instance._avatarGrid.Clear();
+        }
+
+        public static void LoadPage(AvatarAndNamePanel __instance)
+        {
+            ClearPage(__instance);
+            if(pageNumber != 0)
+            {
+                CreateBackButton(__instance);
+            }
+            foreach (string avatarName in pages[pageNumber])
+                __instance.CreateAvatarButton(avatarName);
+            if (pageNumber != maxPageNumber)
+            {
+                CreateForwardButton(__instance);
+            } else if (pages[pageNumber].Count < 20)
+            {
+                int remainder = 20 - pages[pageNumber].Count;
+                if (maxPageNumber == 0)
+                {
+                    remainder++;
+                }
+                for (int i = 0; i < remainder; i++)
+                {
+                    CreateBlankButton(__instance);
+                }
+            }
+        }
+
+        public static void NextPage(AvatarAndNamePanel __instance)
+        {
+            if (pageNumber < maxPageNumber)
+            {
+                pageNumber++;
+                LoadPage(__instance);
+            }
+        }
+
+        public static void PreviousPage(AvatarAndNamePanel __instance)
+        {
+            if (pageNumber > 0)
+            {
+                pageNumber--;
+                LoadPage(__instance);
+            }
+        }
+
+        public static List<string> GetAdditionalAvatarNames()
+        {
+            string absoluteFilePath = GameProvider.Instance.GetApplicationPath("images/sn/icons/avatars/");
+            return ((IEnumerable<string>)Directory.GetFiles(absoluteFilePath, "*", SearchOption.TopDirectoryOnly)).Select<string, string>((Func<string, string>)(file => file.Replace(absoluteFilePath, "avatars/").Replace(".png", ""))).OrderBy<string, string>((Func<string, string>)(x => x)).ToList<string>();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,31 @@
+# CLEVERCRUMBISH'S CAMEO SELECTOR
+A Sunless Sea BepInEx mod that changes the cameo (avatar) selection UI at character creation to one that can handle any number of custom cameos, and makes installing them easier.
+
+### REQUIREMENTS
+• Full version: None.
+• Plugin only version: An existing BepInEx installation in your root Sunless Sea folder
+(install the full version if you don't know what this is).
+
+### Installation
+Download a release. Extract the contents of one of the FOR ROOT FOLDER folders in the zip into your base Sunless Sea folder. On Steam this should be steamapps/common/SunlessSea.
+It will be something similar on GOG/Epic etc. If you already have BepInEx installed on Sunless Sea, choose the PLUGIN ONLY folder to extract the contents of. Otherwise, choose the FULL INSTALL folder.
+
+In either case, Extract the contents of FOR APPDATA FOLDER in the zip into your Sunless Sea folder in Appdata. A common path for this is C:\Users\<Your User Name>\AppData\LocalLow\Failbetter Games\Sunless Sea\
+
+### Usage
+This mod will not do anything if you don't have some custom cameos installed. Some are available on the nexusmods.com, others elsewhere, and you could always make your own.
+
+As in the vanilla game, you can install these cameos in %Appdata%/LocalLow/Failbetter Games/Sunless Sea/images/sn/icons/ and as long as they are .png files with filenames that begin with "avatar_" the game will load them as cameos.
+
+With this mod installed, however, the game will also load any .png image of any name you put in /images/sn/icons/avatars/ as a cameo. This allows you to much more easily keep your custom cameos separate from the default ones if you ever want to delete them. Do not delete or move the "cc_menu" folder in the avatars folder or put any cameos in there. The images in that folder are necessary for the mod to function.
+
+When you get to select a cameo during character creation, click the rightward and leftward pointing hands to move forward and backward in pages of cameos. If the last page has a number of cameos that leaves some space left over, it will generate some blank slots to fill the space and avoid deforming the UI. These can be clicked on, but won't change your selected cameo or do anything else. Otherwise, selecting a cameo works exactly the same as in the base game.
+
+### Uninstallation
+Move any remaining custom cameos you installed from /images/sn/icons/avatars/ to /images/sn/icons/ and rename them to begin with "avatar_" if you want to continue using them. Then delete the "avatars" folder.
+
+Delete the CCCameoSelector.dll file in /BepInEx/plugins/ in your root Sunless Sea folder. If there are then no files in that folder or in the "patchers" folder next to it, you can delete /Bepinex/, winhttp.dll and doorstop_config.ini.
+
+:: CREDITS ::
+♦ Bepis and Horse on the BepInEx modding discord for helping me get this patch running.
+♦ Mayfunction because I stole her BepInEx setup from the Immersive Log Book mod to make absolutely certain it'd be compatible with Sunless Sea.
